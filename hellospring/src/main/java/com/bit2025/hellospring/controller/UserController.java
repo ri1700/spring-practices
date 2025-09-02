2a70d885fc8deebd3b86a46d7e60c425f941db9e
bit <bit@DESKTOP-HPBJEHN>
2025-09-02 11:39:49 +0900

    spring #2

--- hellospring/src/main/java/com/bit2025/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/bit2025/hellospring/controller/UserController.java
@@ -3,6 +3,8 @@ package com.bit2025.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
@@ -29,5 +31,31 @@ public class UserController {
 	public String joinsuccess() {
 		System.out.println("join 처리~");
 		return "/WEB-INF/views/joinsuccess.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(@RequestParam("id") Long id) {
+		/**
+		 * 
+		 * 만일, id라는 Parameter 값이 없거나 값이 올바르지 못하면
+		 * 400 bad request error가 발생한다.
+		 * 
+		 */
+		return "UserController:update(" + id + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/update2", method = RequestMethod.GET)
+	public String update2(@RequestParam(value = "n",required = true, defaultValue = "") String name) {
+		
+		return "UserController:update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(@RequestParam(value = "p",required = true, defaultValue = "1") Integer page) {
+		
+		return "UserController:list(" + page + ")";
 	}
 }
