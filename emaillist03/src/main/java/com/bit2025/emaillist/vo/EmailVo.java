7ed6ab9f0f59216d30d34f3bf81ee58c1a40e28b
bit <bit@DESKTOP-HPBJEHN>
2025-09-08 16:14:56 +0900

    emaillist03 #02

--- emaillist03/src/main/java/com/bit2025/emaillist/vo/EmailVo.java
+++ emaillist03/src/main/java/com/bit2025/emaillist/vo/EmailVo.java
@@ -5,42 +5,43 @@ public class EmailVo {
 	private String firstName;
 	private String lastName;
 	private String email;
-
+	private String regDate;
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
+	}	
+	public String getRegDate() {
+		return regDate;
 	}
-
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
 	@Override
 	public String toString() {
-		return "EmailVo [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+		return "EmailVo [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", regDate=" + regDate + "]";
 	}
-
+	
 }
